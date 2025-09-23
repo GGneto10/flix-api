11099875eb9fa856ed132f5addf02c0d907e0c5d
Cangaro <ggneto1234@gmail.com>
2025-09-23 12:48:44 -0300

    Aula 127 - Crud Completa

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import genre_create_list_view , genre_detail_view
+from genres.views import GenreListCreateView , GenreRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),  
-    path('genres/', genre_create_list_view, name ='genres-create-list'),
-    path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),
+    path('genres/', GenreListCreateView.as_view(), name ='genres-create-list'),
+    path('genres/<int:pk>/',GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,58 +1,11 @@
-from django.http import JsonResponse
-from .models import Genre
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
-import json
 from rest_framework import generics
+from .models import Genre
 from .serializers import GenreSerializer
-
 
 class GenreListCreateView(generics.ListCreateAPIView): 
     queryset = Genre.objects.all() # Define o conjunto de dados para a visualização
     serializer_class = GenreSerializer # Define o serializador a ser usado
-'''
-@csrf_exempt
-def genre_create_list_view(request):
-    if request.method == 'GET':
-        genres = Genre.objects.all()
-        data = [{"id": genre.id, "name": genre.name} for genre in genres]
-        #Isso aqui apenas existe pois o JsonResponse não aceita QuerySets diretamente
-        #E pq eu não estou usando Django Rest Framework.
 
-        return JsonResponse(data, safe=False)
-        #Retorna uma lista de generos em formato JSON
-        #Safe False é necessário para retornar listas
-        #Por padrão, JsonResponse só aceita dicionários
-
-    elif request.method == 'POST':
-        data = json.loads(request.body.decode('utf-8'))
-        new_genre = Genre(name=data.get('name'))
-        new_genre.save() 
-        return JsonResponse({"id": new_genre.id, "name": new_genre.name}, status=201)
-        #Esse 201 indica que um recurso foi criado com sucesso"
-        #Sexosão POST para criar um novo gênero
-
-         #Decodifica o corpo da requisição
-
-        #Aqui você pode adicionar lógica para criar um novo gênero
-    
-'''
-@csrf_exempt
-def genre_detail_view(request, pk):
-
-    genre = get_object_or_404(Genre, pk=pk)
-    if request.method == 'GET': # Retorna os detalhes de um gênero específico
-        data = {"id": genre.id, "name": genre.name} # Dados do gênero
-        return JsonResponse(data) # Retorna os dados em formato JSON
-    
-    elif request.method == 'PUT':   
-        data = json.loads(request.body.decode('utf-8')) # Decodifica o corpo da requisição
-        genre.name = data['name'] # Atualiza o nome do gênero
-        genre.save() # Salva as alterações no banco de dados
-        return JsonResponse({"id": genre.id, "name": genre.name}, # Retorna os dados atualizados
-                            status=203) # Status 203 indica que a requisição foi bem-sucedida e o conteúdo foi modificado
-
-    elif request.method == 'DELETE':
-        genre.delete() # Deleta o gênero do banco de dados
-        return JsonResponse({'message': 'Gênero excluido com êxito.'}, status=204) # Retorna uma resposta vazia com status 204 (No Content)
-        #Status 204 indica que a requisição foi bem-sucedida, mas não há conteúdo para retornar.
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all() # Define o conjunto de dados para a visualização
+    serializer_class = GenreSerializer # Define o serializador a ser usado
