fdfb7a9a3451cccf69dd33978555d08a43c9e1df
Cangaro <ggneto1234@gmail.com>
2025-10-10 14:17:56 -0300

    Atualização do models movies

--- movies/models.py
+++ movies/models.py
@@ -7,9 +7,12 @@ class Movie(models.Model):
 
     title = models.CharField(max_length=200)
     release_date = models.DateField(null=True, blank=True)
+    Br_release_date = models.DateField(null=True, blank=True)
     genre = models.ForeignKey(Genre, on_delete=models.PROTECT, related_name='movies')
     actors = models.ManyToManyField(Actor, related_name='movies')
     resume = models.TextField(blank=True, null=True)
+
+
     #ManyToManyField -
     #Resumindo: um filme pode ter vários atores e um ator pode estar em vários filmes.
     def __str__(self):
