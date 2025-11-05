362df9e20a51c26bb94a724f05df26569efcdc62
Cangaro <ggneto1234@gmail.com>
2025-11-05 14:42:28 -0300

    Proteção das views genres com IsAuthenticated

--- genres/views.py
+++ genres/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
 from .models import Genre
 from .serializers import GenreSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class GenreListCreateView(generics.ListCreateAPIView): 
+    permission_classes = (IsAuthenticated,)
     queryset = Genre.objects.all() # Define o conjunto de dados para a visualização
     serializer_class = GenreSerializer # Define o serializador a ser usado
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Genre.objects.all() # Define o conjunto de dados para a visualização
     serializer_class = GenreSerializer # Define o serializador a ser usado
