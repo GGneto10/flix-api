c94017cbf644e202714f706a32403b09ec707555
Cangaro <ggneto1234@gmail.com>
2025-11-05 14:41:21 -0300

    proteção de permissão nas views actors com IsAuthenticated

--- actors/views.py
+++ actors/views.py
@@ -1,11 +1,15 @@
 from rest_framework import generics
 from .models import Actor
 from .serializers import ActorSerializer
+from rest_framework.permissions import IsAuthenticated # Importa a classe de permissão
+
 
 class ActorListCreateView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)  # Aplica a permissão de autenticação
     queryset = Actor.objects.all()  # Define o conjunto de dados para a visualização
     serializer_class = ActorSerializer  # Define o serializador a ser usado
 
 class ActorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Actor.objects.all()  # Define o conjunto de dados para a visualização
     serializer_class = ActorSerializer  # Define o serializador a ser usado
