1d6af65842d977a30b80d95fa7ca9cde0e306372
Cangaro <ggneto1234@gmail.com>
2025-11-10 14:04:12 -0300

    completando e configurção a Permission Class de Genres. Parte de CRUD de Genres protegida.

--- genres/permissions.py
+++ genres/permissions.py
@@ -3,7 +3,24 @@ from rest_framework import permissions
 class GenrePermissionClass(permissions.BasePermission):
 
     def has_permission(self, request, view):
-       return True  # Allow all users to access the view
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+           return request.user.has_perm('genres.view_genre')
+            # Permitir GET se o usuário tiver permissão de visualização.
+
+        if request.method == 'POST':
+            return request.user.has_perm('genres.add_genre')
+             # Permitir POST se o usuário tiver permissão de adição.  
+
+        if request.method in ['PUT', 'PATCH']:
+            return request.user.has_perm('genres.change_genre')
+             # Permitir PUT/PATCH se o usuário tiver permissão de alteração.
+        
+
+        if request.method == 'DELETE':
+            return request.user.has_perm('genres.delete_genre')
+             # Permitir DELETE se o usuário tiver permissão de exclusão.
+    
+        return False
 
 
 
--- genres/views.py
+++ genres/views.py
@@ -4,11 +4,11 @@ from .serializers import GenreSerializer
 from rest_framework.permissions import IsAuthenticated
 from .permissions import GenrePermissionClass
 class GenreListCreateView(generics.ListCreateAPIView): 
-    permission_classes = (IsAuthenticated, GenrePermissionClass)
+    permission_classes = (IsAuthenticated, GenrePermissionClass,)
     queryset = Genre.objects.all() # Define o conjunto de dados para a visualização
     serializer_class = GenreSerializer # Define o serializador a ser usado
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GenrePermissionClass,)
     queryset = Genre.objects.all() # Define o conjunto de dados para a visualização
     serializer_class = GenreSerializer # Define o serializador a ser usado
