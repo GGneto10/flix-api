54b570172fdf26bbf255b7227e432f8c0ebc5ae4
Cangaro <ggneto1234@gmail.com>
2025-11-26 14:22:22 -0300

    Aula 163 - Além do CRUD, criando uma API Views de status de movies

--- movies/urls.py
+++ movies/urls.py
@@ -4,4 +4,5 @@ from .views import MovieListCreateView, MovieRetrieveUpdateDestroyView
 urlpatterns = [
     path('movies/', MovieListCreateView.as_view(), name='movies-create-list'),
     path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
+    path('movies/stats/', MovieStatsView.as_view(), name='movie-stats-view'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,10 @@
-from rest_framework import generics
+from rest_framework import generics, views
 from .models import Movie
 from .serializers import MovieSerializer
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermission
+from django.db.models import Avg
+from rest_framework.response import Response
 
 class MovieListCreateView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
@@ -14,3 +16,16 @@ class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
+class MovieStatsView(views.APIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
+    queryset = Movie.objects.all()
+
+
+    def get(self, request, format=None):
+        total_movies = Movie.objects.count()
+        average_rating = Movie.objects.aggregate(Avg('rating'))['rating__avg']
+        stats = {
+            'total_movies': total_movies,
+            'average_rating': average_rating,
+        }
+        return Response(stats)
